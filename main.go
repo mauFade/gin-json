f29b75dffd13ecc50cc4db8d5173247c4df47df0
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-22 14:56:16 -0300

    feat: add initializers

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mauFade/gin-json/initializers"
+)
 
 // CompileDaemon -command="./gin-json"
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
 	r := gin.Default()
@@ -12,5 +19,6 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	r.Run()
 }
